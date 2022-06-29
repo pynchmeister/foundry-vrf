9bb5a53533497b9a1c9170f2ebfab53cd2b7d799
Harris Levine <acyclicgraphs@gmail.com>
2022-06-29 12:58:11 -0400

    Add contract & test skeletons

---
+++ src/VerifiableRandomFunction.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.13;
+
+/// @title Verifiable Random Function (VRF) -- generates a random number in Solidity
+/// @author acyclicgraphs@gmail.com
+contract VerifiableRandomFunction {
+
+
+}
